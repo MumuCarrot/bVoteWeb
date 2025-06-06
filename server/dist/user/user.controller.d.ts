8d49685e03cc521c474bc0aa0d238894f6e2d807
vanichka <138333895+MumuCarrot@users.noreply.github.com>
2025-06-06 19:12:12 +0200

    cleaning

--- server/dist/user/user.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
-export declare class UserController {
-    private readonly userService;
-    constructor(userService: UserService);
-    login(createUserDto: CreateUserDto): Promise<any>;
-    register(createUserDto: CreateUserDto): Promise<any>;
-    changeUserData(createUserDto: CreateUserDto): Promise<any>;
-}
